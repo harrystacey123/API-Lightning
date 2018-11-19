f4c576c8a57a9df9980abfc6431467123f3d2f6b
harrystacey123 <harrystacey@live.co.uk>
2018-11-19 09:19:31 -0800

    finally nearing the finished thing

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,18 @@
 var triviaURL = `https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple`;
 
+function shuffle (arr) {
+    new_arr = Array();
+
+    while (arr.length > 0) {
+        idx = Math.round(Math.random() * (arr.length - 1));
+        element = arr[idx];
+        arr[idx] = arr[arr.length - 1];
+        arr.pop();
+        new_arr.push(element);
+    }
+    return new_arr;
+}
+
 $(document).ready(function() {
     console.log(`Success`);
 
@@ -11,30 +24,47 @@ $(document).ready(function() {
             response.results.forEach(element => { console.log(element);
                 var quest = `<p>${element.question}</p>`
                 $(`.harry`).append(quest);
-                var wrongAnswer0 = `<li><button>${element.incorrect_answers[0]}</button></li>`;
-                var wrongAnswer1 = `<li><button>${element.incorrect_answers[1]}</button></li>`;
-                var wrongAnswer2 = `<li><button>${element.incorrect_answers[2]}</button></li>`;
-                var correctAnswer = `<li><button>${element.correct_answer}</button></li>`;
-                $(`.harry`).append(wrongAnswer0);
-                $(`.harry`).append(wrongAnswer1);
-                $(`.harry`).append(wrongAnswer2);
-                $(`.harry`).append(correctAnswer);
-                var i = 0;
-                $('.harry').find('li').each(function(){
-                if(i<4){
-                i++;
+                answers = Array();
+                for (i = 0; i < element.incorrect_answers.length; i++) {
+                    answers.push({'text': element.incorrect_answers[i], 'correct': false});
                 }
-                else{
-                i = 1;
+                answers.push({'text': element.correct_answer, 'correct': true});
+                answers = shuffle(answers);
+
+                for (i = 0; i < answers.length; i++) {
+                    btt = $('<button>', {html: answers[i].text});
+                    $('.harry').append(`<li>`);
+                    $('.harry').append(btt);
+                    $('.harry').append(`</li>`);
+                    if (answers[i].correct) {
+                        btt.on(`click`, function() {
+                            scoreTotal += 1;
+                            console.log(scoreTotal);
+                            $(`.next`).html(`Score: ` + scoreTotal);
+                        })
+                    }
+
                 }
-                $(this).addClass('class-' + i);
-                });
             })
             var scoreTotal = 0;
-            $(`.class-4`).on(`click`, function() {
-                scoreTotal += 1;
-                console.log(scoreTotal);
-            })
+            $()
         }
     })
+    window.twttr = (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0],
+            t = window.twttr || {};
+        if (d.getElementById(id)) return t;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    
+        t._e = [];
+        t.ready = function(f) {
+            t._e.push(f);
+        };
+    
+        return t;
+    }(document, "script", "twitter-wjs"));
 })
+
